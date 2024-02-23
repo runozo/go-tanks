069c0849a4f6599db3e15c8d15514ff375805f58
Ghiroz <cghirelli@arpae.it>
2024-02-23 22:14:17 +0100

    wfc nearly complete

--- tileviewer/main.go
+++ tileviewer/main.go
@@ -26,6 +26,49 @@ const (
 	tileHeight   = 64
 )
 
+var tileOptions = map[string][]int{
+	"tileGrass1.png":                     {0, 0, 0, 0}, // 0 grass
+	"tileGrass2.png":                     {0, 0, 0, 0},
+	"tileGrass_roadCornerLL.png":         {0, 0, 1, 1}, // 1 road with grass
+	"tileGrass_roadCornerLR.png":         {0, 1, 1, 0},
+	"tileGrass_roadCornerUL.png":         {1, 0, 0, 1},
+	"tileGrass_roadCornerUR.png":         {1, 1, 0, 0},
+	"tileGrass_roadCrossing.png":         {1, 1, 1, 1},
+	"tileGrass_roadCrossingRound.png":    {1, 1, 1, 1},
+	"tileGrass_roadEast.png":             {0, 1, 0, 1},
+	"tileGrass_roadNorth.png":            {1, 0, 1, 0},
+	"tileGrass_roadSplitE.png":           {1, 1, 1, 0},
+	"tileGrass_roadSplitN.png":           {1, 1, 0, 1},
+	"tileGrass_roadSplitS.png":           {0, 1, 1, 1},
+	"tileGrass_roadSplitW.png":           {1, 0, 1, 1},
+	"tileGrass_roadTransitionE.png":      {4, 3, 4, 1}, //
+	"tileGrass_roadTransitionE_dirt.png": {4, 3, 4, 1},
+	"tileGrass_roadTransitionN.png":      {3, 6, 1, 6},
+	"tileGrass_roadTransitionN_dirt.png": {3, 6, 1, 6},
+	"tileGrass_roadTransitionS.png":      {1, 8, 3, 8},
+	"tileGrass_roadTransitionS_dirt.png": {1, 8, 3, 8},
+	"tileGrass_roadTransitionW.png":      {5, 1, 5, 3},
+	"tileGrass_roadTransitionW_dirt.png": {5, 1, 5, 3},
+	"tileGrass_transitionE.png":          {4, 2, 4, 0},
+	"tileGrass_transitionN.png":          {2, 6, 0, 6},
+	"tileGrass_transitionS.png":          {0, 8, 2, 8},
+	"tileGrass_transitionW.png":          {5, 0, 5, 2},
+	"tileSand1.png":                      {2, 2, 2, 2},
+	"tileSand2.png":                      {2, 2, 2, 2},
+	"tileSand_roadCornerLL.png":          {2, 2, 3, 3},
+	"tileSand_roadCornerLR.png":          {2, 3, 3, 2},
+	"tileSand_roadCornerUL.png":          {3, 2, 2, 3},
+	"tileSand_roadCornerUR.png":          {3, 3, 2, 2},
+	"tileSand_roadCrossing.png":          {3, 3, 3, 3},
+	"tileSand_roadCrossingRound.png":     {3, 3, 3, 3},
+	"tileSand_roadEast.png":              {2, 3, 2, 3},
+	"tileSand_roadNorth.png":             {3, 2, 3, 2},
+	"tileSand_roadSplitE.png":            {3, 3, 3, 2},
+	"tileSand_roadSplitN.png":            {3, 3, 2, 3},
+	"tileSand_roadSplitS.png":            {2, 3, 3, 3},
+	"tileSand_roadSplitW.png":            {3, 2, 3, 3},
+}
+
 var mplusNormalFont font.Face
 var icons []Icon
 
@@ -108,7 +151,11 @@ func (g *Game) Draw(screen *ebiten.Image) {
 			ops := &ebiten.DrawImageOptions{}
 			ops.GeoM.Translate(float64(x), float64(y))
 			screen.DrawImage(icons[i].Img, ops)
-			text.Draw(screen, fmt.Sprintf("%.28s", icons[i].Name), mplusNormalFont, x, y, color.White)
+			text.Draw(screen, fmt.Sprintf("%.28s", icons[i].Name), mplusNormalFont, x, y+12+icons[i].Img.Bounds().Dy()/2, color.White)
+			text.Draw(screen, fmt.Sprintf("%d", tileOptions[icons[i].Name][0]), mplusNormalFont, x+icons[i].Img.Bounds().Dx()/2, y+16, color.Black)
+			text.Draw(screen, fmt.Sprintf("%d", tileOptions[icons[i].Name][1]), mplusNormalFont, x+icons[i].Img.Bounds().Dx()-8, y+icons[i].Img.Bounds().Dy()/2, color.Black)
+			text.Draw(screen, fmt.Sprintf("%d", tileOptions[icons[i].Name][2]), mplusNormalFont, x+icons[i].Img.Bounds().Dx()/2, y+icons[i].Img.Bounds().Dy(), color.Black)
+			text.Draw(screen, fmt.Sprintf("%d", tileOptions[icons[i].Name][3]), mplusNormalFont, x, y+icons[i].Img.Bounds().Dy()/2, color.Black)
 			x = x + w*2
 			if x > screenWidth {
 				x = 20
