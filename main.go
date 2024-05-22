4c859e0f3e7758d082041ac80d91d7fe88022d9d
Ghiroz <cghirelli@arpae.it>
2024-05-22 15:37:06 +0200

    profiling

--- main.go
+++ main.go
@@ -1,12 +1,28 @@
 package main
 
 import (
+	"flag"
+	"log"
+	"os"
+	"runtime/pprof"
+
 	"github.com/hajimehoshi/ebiten/v2"
 
 	"github.com/runozo/go-tanks/game"
 )
 
+var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
+
 func main() {
+	flag.Parse()
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
+	}
 	g := game.NewGame()
 
 	err := ebiten.RunGame(g)
