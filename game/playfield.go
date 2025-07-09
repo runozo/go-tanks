6a5b9749b71858fc3ec9b494bc9da40dd50145f7
Cesare Ghirelli <cghirelli@arpae.it>
2025-07-09 12:34:50 +0200

    uses github.com/runozo/go-wave-function-collapse as wfc renderer

--- game/playfield.go
+++ game/playfield.go
@@ -2,11 +2,10 @@ package game
 
 import (
 	_ "image/png"
-	"log/slog"
-	"time"
 
 	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/runozo/go-tanks/assets"
+	"github.com/runozo/go-wave-function-collapse/assets"
+	"github.com/runozo/go-wave-function-collapse/wfc"
 )
 
 const (
@@ -15,9 +14,7 @@ const (
 )
 
 type Tile struct {
-	collapsed bool
-	image     *ebiten.Image
-	options   []string
+	image *ebiten.Image
 }
 type Playfield struct {
 	width       int
@@ -26,7 +23,6 @@ type Playfield struct {
 	numOfTilesX int
 	numOfTilesY int
 	isRendered  bool
-	assets      *assets.Assets
 }
 
 // NewPlayfield creates a new Playfield with the specified width, height, and assets.
@@ -38,25 +34,31 @@ type Playfield struct {
 //
 // Returns:
 // - a pointer to the newly created Playfield.
-func NewPlayfield(width, height int, assets *assets.Assets) *Playfield {
+func NewPlayfield(width, height int, ass *assets.Assets) *Playfield {
 
 	tilesX := width/tileWidth + 1
 	tilesY := height/tileHeight + 1
 
-	// setup tiles with all the options enabled
-	tiles := make([]Tile, tilesX*tilesY)
-	resetTilesOptions(&tiles)
-
 	playfield := &Playfield{
-		width:       width,
-		height:      height,
-		tiles:       tiles,
+		width:  width,
+		height: height,
+		tiles:  make([]Tile, tilesX*tilesY),
+
 		isRendered:  false,
 		numOfTilesX: tilesX,
 		numOfTilesY: tilesY,
-		assets:      assets,
 	}
-	go renderPlayfield(playfield)
+
+	myWfc := wfc.NewWfc(screenWidth/tileWidth+1, screenHeight/tileHeight+1, ass.TileEntries)
+	myWfc.StartRender()
+	for y := 0; y < tilesY; y++ {
+		for x := 0; x < tilesX; x++ {
+			tile := Tile{
+				image: ass.GetSprite(myWfc.Tiles[y*tilesX+x].Name),
+			}
+			playfield.tiles[y*tilesX+x] = tile
+		}
+	}
 
 	return playfield
 }
@@ -73,65 +75,6 @@ func (p *Playfield) Draw(screen *ebiten.Image) {
 				ops.GeoM.Translate(float64(x), float64(y))
 				screen.DrawImage(p.tiles[i].image, ops)
 				i++
-			}
-		}
-	}
-}
-
-func renderPlayfield(p *Playfield) {
-	startTime := time.Now()
-	defer func() {
-		endTime := time.Now()
-		duration := endTime.Sub(startTime)
-		slog.Info("Rendering of playfield took", "duration", duration)
-	}()
-
-	for !p.isRendered {
-		// pick the minimum entropy indexes
-		minEntropyIndexes := getMinEntropyIndexes(&p.tiles)
-
-		if len(minEntropyIndexes) <= 0 {
-			slog.Info("Playfiled is rendered. No more collapsable cells.", "tiles", len(p.tiles))
-			for i := 0; i < len(p.tiles); i++ {
-				if !p.tiles[i].collapsed {
-					p.tiles[i].image = p.assets.GetSprite(p.tiles[i].options[0])
-					p.tiles[i].collapsed = true
-				}
-			}
-			p.isRendered = true
-		} else {
-			collapsedIndex := collapseRandomCellWithMinEntropy(&p.tiles, &minEntropyIndexes)
-			p.tiles[collapsedIndex].image = p.assets.GetSprite(p.tiles[collapsedIndex].options[0])
-			p.tiles[collapsedIndex].collapsed = true
-
-			for y := 0; y < p.numOfTilesY; y++ {
-				for x := 0; x < p.numOfTilesX; x++ {
-					index := y*p.numOfTilesX + x
-					if len(p.tiles[index].options) == 0 {
-						// we did not found any options, let's restart
-						slog.Info("Restarting!")
-						resetTilesOptions(&p.tiles)
-					}
-
-					if !p.tiles[index].collapsed {
-						// Look UP
-						if y > 0 {
-							p.tiles[index].options = lookAndFilter(ruleUP, ruleDOWN, p.tiles[index].options, p.tiles[(y-1)*p.numOfTilesX+x].options)
-						}
-						// Look RIGHT
-						if x < p.numOfTilesX-1 {
-							p.tiles[index].options = lookAndFilter(ruleRIGHT, ruleLEFT, p.tiles[index].options, p.tiles[y*p.numOfTilesX+x+1].options)
-						}
-						// Look DOWN
-						if y < p.numOfTilesY-1 {
-							p.tiles[index].options = lookAndFilter(ruleDOWN, ruleUP, p.tiles[index].options, p.tiles[(y+1)*p.numOfTilesX+x].options)
-						}
-						// Look LEFT
-						if x > 0 {
-							p.tiles[index].options = lookAndFilter(ruleLEFT, ruleRIGHT, p.tiles[index].options, p.tiles[y*p.numOfTilesX+x-1].options)
-						}
-					}
-				}
 			}
 		}
 	}
