ebcb40d93b73b55192a246bb7a1208b19e61ca3c
runozo <runozo@gmail.com>
2025-07-17 23:07:49 +0200

    bullet ballistic

--- game/bullet.go
+++ game/bullet.go
@@ -35,25 +35,24 @@ func NewBullet(sprite *ebiten.Image, pos Vector, rotation, speed, slope float64)
 
 func (b *Bullet) Update() {
 	if b.altitude > 0.0 {
-		b.elapsedTime += 1.0 / float64(ebiten.TPS())
 		b.position.X += math.Sin(b.rotation) * b.speed
 		b.position.Y += math.Cos(b.rotation) * -b.speed
 		verticalSpeed := b.speed * math.Sin(b.slope)
+		b.altitude += verticalSpeed - gravity*math.Pow(b.elapsedTime, 2)
+		b.elapsedTime += 1.0 / float64(ebiten.TPS())
 		b.slope = verticalSpeed / b.speed
-		b.altitude += verticalSpeed*b.elapsedTime - gravity*math.Pow(b.elapsedTime, 2)
 		// fmt.Println(verticalSpeed, b.slope, b.altitude, b.elapsedTime)
 	}
 }
 
 func (b *Bullet) Draw(screen *ebiten.Image) {
 	if b.altitude > 0.0 {
-		scale := b.altitude * 0.1
+		scale := b.altitude * 0.2
 		if scale < 1.0 {
 			scale = 1.0
 		}
 		op := &ebiten.DrawImageOptions{}
 		op.GeoM.Rotate(b.rotation)
-
 		op.GeoM.Scale(scale, scale)
 		op.GeoM.Translate(b.position.X, b.position.Y)
 		screen.DrawImage(b.sprite, op)
