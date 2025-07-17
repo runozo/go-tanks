081b90e9266ccca41cef4240be526fda33d0ef02
Cesare Ghirelli <cghirelli@arpae.it>
2025-07-17 22:18:45 +0200

    bullet ballistic

--- game/bullet.go
+++ game/bullet.go
@@ -7,29 +7,55 @@ import (
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
+const (
+	gravity = 3.0
+)
+
 type Bullet struct {
-	position Vector
-	rotation float64
-	sprite   *ebiten.Image
-	speed    float64
+	position    Vector
+	rotation    float64
+	sprite      *ebiten.Image
+	speed       float64
+	slope       float64
+	altitude    float64
+	elapsedTime float64
 }
 
-func NewBullet(sprite *ebiten.Image, pos Vector, rotation, speed float64) *Bullet {
+func NewBullet(sprite *ebiten.Image, pos Vector, rotation, speed, slope float64) *Bullet {
 	return &Bullet{
-		position: pos,
-		rotation: rotation,
-		sprite:   sprite,
-		speed:    speed,
+		position:    pos,
+		rotation:    rotation,
+		sprite:      sprite,
+		speed:       speed,
+		slope:       slope,
+		altitude:    0.2,
+		elapsedTime: 0.0,
 	}
 }
 
 func (b *Bullet) Update() {
-	b.position.X += math.Sin(b.rotation) * b.speed
-	b.position.Y += math.Cos(b.rotation) * -b.speed
+	if b.altitude > 0.0 {
+		b.elapsedTime += 1.0 / float64(ebiten.TPS())
+		b.position.X += math.Sin(b.rotation) * b.speed
+		b.position.Y += math.Cos(b.rotation) * -b.speed
+		verticalSpeed := b.speed * math.Sin(b.slope)
+		b.slope = verticalSpeed / b.speed
+		b.altitude += verticalSpeed*b.elapsedTime - gravity*math.Pow(b.elapsedTime, 2)
+		// fmt.Println(verticalSpeed, b.slope, b.altitude, b.elapsedTime)
+	}
 }
 
 func (b *Bullet) Draw(screen *ebiten.Image) {
-	op := &ebiten.DrawImageOptions{}
-	op.GeoM.Translate(b.position.X, b.position.Y)
-	screen.DrawImage(b.sprite, op)
+	if b.altitude > 0.0 {
+		scale := b.altitude * 0.1
+		if scale < 1.0 {
+			scale = 1.0
+		}
+		op := &ebiten.DrawImageOptions{}
+		op.GeoM.Rotate(b.rotation)
+
+		op.GeoM.Scale(scale, scale)
+		op.GeoM.Translate(b.position.X, b.position.Y)
+		screen.DrawImage(b.sprite, op)
+	}
 }
