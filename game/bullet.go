af17e2e39340427ccdc8b82691fb71a473403c19
runozo <runozo@gmail.com>
2025-07-25 14:27:18 +0200

    bellet range

--- game/bullet.go
+++ game/bullet.go
@@ -9,24 +9,26 @@ import (
 )
 
 const (
-	gravity           = 3.0
+	gravity           = 8.0
 	bulletSpawnOffset = 20.0
 	bulletSpeed       = 10.0
+	bulletMinScale    = 1.0
+	bulletMaxScale    = 8.0
 )
 
 type Bullet struct {
-	position     Vector
-	rotation     float64
-	sprite       *ebiten.Image
-	speed        float64
-	slope        float64
-	altitude     float64
-	scale        float64
-	elapsedTime  float64
-	spriteWidth  float64
-	spriteHeight float64
-	barrelWidth  float64
-	barrelHeight float64
+	position      Vector
+	rotation      float64
+	sprite        *ebiten.Image
+	verticalSpeed float64
+	slope         float64
+	altitude      float64
+	scale         float64
+	elapsedTime   float64
+	spriteWidth   float64
+	spriteHeight  float64
+	barrelWidth   float64
+	barrelHeight  float64
 }
 
 func NewBullet(barrel *Barrel) *Bullet {
@@ -41,33 +43,37 @@ func NewBullet(barrel *Barrel) *Bullet {
 	fmt.Println("Barrel position", barrel.position.X, barrel.position.Y, "Bullet position", position.X, position.Y)
 
 	return &Bullet{
-		position:     position,
-		rotation:     barrel.absoluteRotation,
-		sprite:       barrel.bulletSprite,
-		speed:        bulletSpeed,
-		slope:        barrel.slope,
-		altitude:     0.2,
-		scale:        1.0,
-		elapsedTime:  0.0,
-		spriteWidth:  spriteWidth,
-		spriteHeight: spriteHeight,
-		barrelWidth:  barrel.spriteWidth,
-		barrelHeight: barrel.spriteHeight,
+		position:      position,
+		rotation:      barrel.absoluteRotation,
+		sprite:        barrel.bulletSprite,
+		verticalSpeed: bulletSpeed * math.Sin(barrel.slope),
+		slope:         barrel.slope,
+		altitude:      0.2,
+		scale:         bulletMinScale,
+		elapsedTime:   0.0,
+		spriteWidth:   spriteWidth,
+		spriteHeight:  spriteHeight,
+		barrelWidth:   barrel.spriteWidth,
+		barrelHeight:  barrel.spriteHeight,
 	}
 }
 
 func (b *Bullet) Update() {
 	if b.altitude > 0.0 {
-		b.scale = b.altitude * 0.2
-		if b.scale < 1.0 {
-			b.scale = 1.0
+		b.position.X += math.Sin(b.rotation) * bulletSpeed
+		b.position.Y -= math.Cos(b.rotation) * bulletSpeed
+		b.altitude += b.verticalSpeed*b.elapsedTime - 0.5*gravity*math.Pow(b.elapsedTime, 2)
+		b.scale = b.altitude
+		if b.scale < bulletMinScale {
+			b.scale = bulletMinScale
 		}
-		b.position.X += math.Sin(b.rotation) * b.speed
-		b.position.Y -= math.Cos(b.rotation) * b.speed
-		verticalSpeed := b.speed * math.Sin(b.slope)
-		b.altitude += verticalSpeed - gravity*math.Pow(b.elapsedTime, 2)
+		/*
+			if b.scale > bulletMaxScale {
+				b.scale = bulletMaxScale
+			}
+		*/
 		b.elapsedTime += 1.0 / float64(ebiten.TPS())
-		b.slope = verticalSpeed / b.speed
+		// b.slope = b.verticalSpeed / bulletSpeed
 		// fmt.Println(verticalSpeed, b.slope, b.altitude, b.elapsedTime)
 	}
 }
@@ -77,13 +83,13 @@ func (b *Bullet) Draw(screen *ebiten.Image) {
 		bulletHalfW := b.spriteWidth / 2
 		bulletAndBarrellheight := b.barrelHeight + b.spriteHeight
 
-		fmt.Println("Bullet altitude", b.altitude, "Scale", b.scale)
+		fmt.Println(b.altitude) // , "Scale", b.scale)
 
 		op := &ebiten.DrawImageOptions{}
-		op.GeoM.Translate(-bulletHalfW*b.scale, -bulletAndBarrellheight*b.scale)
+		op.GeoM.Translate(-bulletHalfW, -bulletAndBarrellheight)
 		op.GeoM.Rotate(b.rotation)
-		op.GeoM.Translate(bulletHalfW*b.scale, bulletAndBarrellheight*b.scale)
 		op.GeoM.Scale(b.scale, b.scale)
+		op.GeoM.Translate(bulletHalfW, bulletAndBarrellheight)
 		op.GeoM.Translate(b.position.X, b.position.Y)
 		screen.DrawImage(b.sprite, op)
 	}
