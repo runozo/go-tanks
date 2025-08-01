b54cdc69c85ec36b6834801d2d21e399f6d3aa41
runozo <runozo@gmail.com>
2025-08-01 14:50:50 +0200

    new tank spawn from old position

--- game/tank.go
+++ game/tank.go
@@ -16,13 +16,13 @@ type Tank struct {
 	rotation   float64
 }
 
-func NewTank(game *Game, bodySprite, barrelSprite, bulletSprite *ebiten.Image) *Tank {
+func NewTank(game *Game, bodySprite, barrelSprite, bulletSprite *ebiten.Image, x, y, rotation float64) *Tank {
 	tank := &Tank{
 		bodySprite: bodySprite,
 		bodyWidth:  float64(bodySprite.Bounds().Dx()),
 		bodyHeight: float64(bodySprite.Bounds().Dy()),
-		position:   Vector{X: screenWidth / 2, Y: screenHeight / 2},
-		rotation:   0.0,
+		position:   Vector{X: x, Y: y},
+		rotation:   rotation,
 		barrel:     nil,
 	}
 	shootAnimationSprites := []*ebiten.Image{
@@ -46,7 +46,7 @@ func NewTank(game *Game, bodySprite, barrelSprite, bulletSprite *ebiten.Image) *
 	return tank
 }
 
-func NewRandomTank(game *Game) *Tank {
+func NewRandomTank(game *Game, x, y, rotation float64) *Tank {
 	bodies := []string{"tankBody_red_outline", "tankBody_blue_outline", "tankBody_dark_outline", "tankBody_green_outline", "tankBody_dark_outline", "tankBody_green_outline", "tankBody_sand_outline"}
 	barrels := []string{"tankDark_barrel1_outline", "tankDark_barrel2_outline", "tankDark_barrel3_outline", "tankGreen_barrel1", "tankGreen_barrel1_outline", "tankGreen_barrel2", "tankGreen_barrel2_outline", "tankGreen_barrel3", "tankGreen_barrel3_outline", "tankRed_barrel1", "tankRed_barrel1_outline", "tankRed_barrel2_outline", "tankRed_barrel3_outline", "tankSand_barrel2_outline", "tankSand_barrel3_outline"}
 	bullets := []string{"bulletSand3_outline", "bulletGreen3_outline", "bulletBlue3_outline"}
@@ -57,7 +57,7 @@ func NewRandomTank(game *Game) *Tank {
 
 	fmt.Println("Random tank:", randomBodyName, randomBarrelName, randomBulletName)
 
-	return NewTank(game, game.assets.GetSprite(randomBodyName), game.assets.GetSprite(randomBarrelName), game.assets.GetSprite(randomBulletName))
+	return NewTank(game, game.assets.GetSprite(randomBodyName), game.assets.GetSprite(randomBarrelName), game.assets.GetSprite(randomBulletName), x, y, rotation)
 }
 
 func (t *Tank) Fire() *Bullet {
